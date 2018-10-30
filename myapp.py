bfcb78408e546032cbc3ba717ef1ba745ebe7edb
micro <syednaveedjalal@gmail.com>
2018-10-30 12:22:12 +0200

    Variables

--- myapp.py
+++ myapp.py
@@ -1,4 +1,8 @@
-print("   / |")
-print("  /  |")
-print(" /   |")
-print("/____|")
+product_name= "B-oil"
+product_usetime= "5"
+
+
+print("Apply " + product_name + " few drops daily or as needed to entire ")
+print(" 5 times face ideally after water-based treatments.")
+print(" Great oil!!! light, absorbs quickly, doesn’t feel heavy or sticky, makes my skin glow!")
+print(" B-oil Smell isn’t the best, but use 5 times a day")
